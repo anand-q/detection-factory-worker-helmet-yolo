31c8b5966b6e09355c7897591060e37c1fbf25f3
anand-q <anandramesh00@gmail.com>
2025-08-03 18:52:11 +0530

    Adding yolo11n.pt

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
 
-model = YOLO(model="./models/yolov8n.pt")
+model = YOLO(model="./models/yolov11.pt")
 model.train(data='data.yaml', epochs = 50, imgsz = 640, batch = 16)
