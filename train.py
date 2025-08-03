8cb0a1c8a45d2a1cf808c098898a85b53b8a45b7
anand-q <anandramesh00@gmail.com>
2025-08-03 19:10:02 +0530

    modified train.py for correct code characters (1)

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
 
-model = YOLO(model="./models/yolov11.pt")
+model = YOLO(model="./models/yolo11n.pt")
 model.train(data='data.yaml', epochs = 50, imgsz = 640, batch = 16)
